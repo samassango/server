4898423b866e7b0aca0ffedaf1e991119bda6088
Sibusiso Masango <SibusisoMa@statssa.gov.za>
2021-11-23 13:00:07 +0200

    new changes

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,10 @@ import { PeopleAPI } from './graphql/people-api';
 import { ApolloServer, gql } from 'apollo-server';
 import { typeDefs } from './graphql/schema';
 import { resolvers } from './graphql/resolvers';
-import { BaseRedisCache } from 'apollo-server-cache-redis';
-import Redis from 'ioredis';
 const server = new ApolloServer({
     typeDefs, 
     resolvers,
     introspection:true,
-    // cache: new BaseRedisCache({
-    //     client: new Redis({
-    //       host: 'redis-server',
-    //     }),
-    //   }),
     dataSources: () => ({
         peopleAPI: new PeopleAPI()
       })});
