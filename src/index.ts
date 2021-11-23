0a12ecb8d9d886880dc5e0c0d3b6652c105a3799
Sibusiso Masango <SibusisoMa@statssa.gov.za>
2021-11-23 12:07:47 +0200

    Implementing the memory cache for performance

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import { PeopleAPI } from './graphql/people-api';
 import { ApolloServer, gql } from 'apollo-server';
 import { typeDefs } from './graphql/schema';
 import { resolvers } from './graphql/resolvers';
-
+import { BaseRedisCache } from 'apollo-server-cache-redis';
+import Redis from 'ioredis';
 const server = new ApolloServer({
     typeDefs, 
     resolvers,
+    cache: new BaseRedisCache({
+        client: new Redis({
+          host: 'redis-server',
+        }),
+      }),
     dataSources: () => ({
         peopleAPI: new PeopleAPI()
       })});
